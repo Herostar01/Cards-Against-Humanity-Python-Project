89d36fa585c9c17236000aef2a7b510a5ab1ce95
Muhammad Shahbaz <88650403+Herostar01@users.noreply.github.com>
2022-01-10 16:04:40 -0800

    Final Project Version Upload

---
+++ flask_app/static/js/Cah.js
@@ -0,0 +1,799 @@
+export const Cah = 
+[
+  {
+    "name": "Main",
+    "white": [
+      {
+        "text": "A belly full of hard-boiled eggs.",
+        "pack": 0
+      },
+      {
+        "text": "Jonathan in a suit riding a bicycle for GOD!",
+        "pack": 0
+      },
+      {
+        "text": "A joyless vegan party.",
+        "pack": 0
+      },
+      {
+        "text": "A sobering quantity of chili cheese fries.",
+        "pack": 0
+      },
+      {
+        "text": "A table for one at The Cheesecake Factory.",
+        "pack": 0
+      },
+      {
+        "text": "Being psychologically scarred by Gordon Ramsay.",
+        "pack": 0
+      },
+      {
+        "text": "Clamping down on a gazelle's jugular and tasting its warm life waters.",
+        "pack": 0
+      },
+      {
+        "text": "Committing suicide at the Old Country Buffet.",
+        "pack": 0
+      },
+      {
+        "text": "Faulty UIDs.",
+        "pack": 0
+      },
+      {
+        "text": "Going vegetarian and feeling so great all the time.",
+        "pack": 0
+      },
+      {
+        "text": "Mustard Twinkies.",
+        "pack": 0
+      },
+      {
+        "text": "Kale farts.",
+        "pack": 0
+      },
+      {
+        "text": "Kevin Bacon Bits.",
+        "pack": 0
+      },
+      {
+        "text": "Licking the cake batter off of grandma's fingers.",
+        "pack": 0
+      },
+      {
+        "text": "Not knowing what to believe anymore about butter.",
+        "pack": 0
+      },
+      {
+        "text": "Oreos for dinner.",
+        "pack": 0
+      },
+      {
+        "text": "Real cheese flavor.",
+        "pack": 0
+      },
+      {
+        "text": "Soup.",
+        "pack": 0
+      },
+      {
+        "text": "Sucking down thousands of pounds of krill every day.",
+        "pack": 0
+      },
+      {
+        "text": "Swishing the wine around and sniffing it like a big fancy man.",
+        "pack": 0
+      },
+      {
+        "text": "The Dial-A-Slice Apple Divider from Williams-Sonoma.",
+        "pack": 0
+      },
+      {
+        "text": "The Hellman's Mayonnaise Corporation.",
+        "pack": 0
+      },
+      {
+        "text": "The hot dog I put in my butthole ten days ago.",
+        "pack": 0
+      },
+      {
+        "text": "The inaudible screams of carrots.",
+        "pack": 0
+      },
+      {
+        "text": "What to do with all of this chocolate on my face.",
+        "pack": 0
+      },
+      {
+        "text": "A fully-dressed female videogame character.",
+        "pack": 1
+      },
+      {
+        "text": "A grumpy old Harrison Ford who'd rather be doing anything else.",
+        "pack": 1
+      },
+      {
+        "text": "A homemade, sweat-stained Star Trek Uniform.",
+        "pack": 1
+      },
+      {
+        "text": "Achieving 500 actions per minute.",
+        "pack": 1
+      },
+      {
+        "text": "Charging up all the way.",
+        "pack": 1
+      },
+      {
+        "text": "Eating a pizza that's lying in the street to gain health.",
+        "pack": 1
+      },
+      {
+        "text": "Endless ninjas.",
+        "pack": 1
+      },
+      {
+        "text": "Forgetting to eat, and consequently dying.",
+        "pack": 1
+      },
+      {
+        "text": "Getting bitten by a radioactive spider and then battling leukemia for 30 years.",
+        "pack": 1
+      },
+      {
+        "text": "KHAAAAAAAAN!",
+        "pack": 1
+      },
+      {
+        "text": "Loading from a previous save.",
+        "pack": 1
+      },
+      {
+        "text": "Offering your only child for an ore and a sheep.",
+        "pack": 1
+      },
+      {
+        "text": "Running out of stamina.",
+        "pack": 1
+      },
+      {
+        "text": "Separate drinking fountains for dark elves.",
+        "pack": 1
+      },
+      {
+        "text": "Flynn's room of cadavers.",
+        "pack": 1
+      },
+      {
+        "text": "Sharpening a foam broadsword on a foam whetstone.",
+        "pack": 1
+      },
+      {
+        "text": "Forcing my tongue into a Sega Genesis and pressing the power button.",
+        "pack": 1
+      },
+      {
+        "text": "Taking 2d6 emotional damage.",
+        "pack": 1
+      },
+      {
+        "text": "Muhammad's seventh visit to the Starbucks restroom.",
+        "pack": 1
+      },
+      {
+        "text": "One ring to rule them all.",
+        "pack": 1
+      },
+      {
+        "text": "The collective wail of every Magic player suddenly realizing that they've spent hundreds of dollars on pieces of cardboard.",
+        "pack": 1
+      },
+      {
+        "text": "The depression that ensues after catching 'em all.",
+        "pack": 1
+      },
+      {
+        "text": "Yoshi's huge egg-laying cloaca.",
+        "pack": 1
+      },
+      {
+        "text": "A picture of Sean Connery shirtless",
+        "pack": 2
+      },
+      {
+        "text": "A hazmat suit full of farts.",
+        "pack": 2
+      },
+      {
+        "text": "A misty room full of glistening egg sacs.",
+        "pack": 2
+      },
+      {
+        "text": "A planet-devouring space worm named Beetlejuice.",
+        "pack": 2
+      },
+      {
+        "text": "Pablo getting Chick-fil-A at 10pm.",
+        "pack": 2
+      },
+      {
+        "text": "A protagonist with no qualities.",
+        "pack": 2
+      },
+      {
+        "text": "An alternate history where Hitler was gay but he still killed all those people.",
+        "pack": 2
+      },
+      {
+        "text": "Beep beep boop beep boop.",
+        "pack": 2
+      },
+      {
+        "text": "Cheerful organ-harvesting robots.",
+        "pack": 2
+      },
+      {
+        "text": "Cosmic bowling.",
+        "pack": 2
+      },
+      {
+        "text": "Tyler's wind tunnel torture technique.",
+        "pack": 2
+      },
+      {
+        "text": "Frantically writing equations on a chalkboard.",
+        "pack": 2
+      },
+      {
+        "text": "Funkified aliens from the planet Groovius.",
+        "pack": 2
+      },
+      {
+        "text": "Going too far with science and bad things happening.",
+        "pack": 2
+      },
+      {
+        "text": "How great of a movie Men In Black was.",
+        "pack": 2
+      },
+      {
+        "text": "Laying thousands of eggs in a man's colon.",
+        "pack": 2
+      },
+      {
+        "text": "Realizing every person's average age in heaven is sixty.",
+        "pack": 2
+      },
+      {
+        "text": "Nine seasons of sexual tension with David Duchovny.",
+        "pack": 2
+      },
+      {
+        "text": "That girl from the Hunger Games.",
+        "pack": 2
+      },
+      {
+        "text": "The dystopia we're living in right now.",
+        "pack": 2
+      },
+      {
+        "text": "The ending of Lost.",
+        "pack": 2
+      },
+      {
+        "text": "Christian's zoom lagging...again.",
+        "pack": 2
+      },
+      {
+        "text": "Chewbacca's dingleberries.",
+        "pack": 2
+      },
+      {
+        "text": "Getting splinters on your bum from riding a Nimbus 3000.",
+        "pack": 2
+      },
+      {
+        "text": "A GIANT FREAKIN' DEATH FRAY!!!",
+        "pack": 2
+      },
+      {
+        "text": "Thor, the God of thunder and love.",
+        "pack": 2
+      },
+      {
+        "text": "Barrack Obama's stimulus package.",
+        "pack": 2
+      },
+      {
+        "text": "A truck full of rednecks with shotguns.",
+        "pack": 2
+      },
+      {
+        "text": "Samuel Jackson tired of these goddamn snakes on this goddamn plane.",
+        "pack": 2
+      },
+      {
+        "text": "Whispering that you are Batman.",
+        "pack": 2
+      },
+      {
+        "text": "Hurricane Katrina.",
+        "pack": 2
+      },
+      {
+        "text": "Joe Biden giving little girls a free neck massage.",
+        "pack": 2
+      },
+      {
+        "text": "A gentle caress of the inner thigh.",
+        "pack": 2
+      },
+      {
+        "text": "A chloroform-soaked rag.",
+        "pack": 2
+      },
+      {
+        "text": "Monthly child support payments.",
+        "pack": 2
+      },
+      {
+        "text": "A passionate Latino lover.",
+        "pack": 2
+      },
+      {
+        "text": "Giving money to a Nigerian prince.",
+        "pack": 2
+      },
+      {
+        "text": "An ancient Indian burial ground.",
+        "pack": 2
+      },
+      {
+        "text": "Too much marijuana.",
+        "pack": 2
+      },
+      {
+        "text": "Breeding cows for their delicious flesh.",
+        "pack": 2
+      },
+      {
+        "text": "Waiting in line at the DMV.",
+        "pack": 2
+      },
+      {
+        "text": "Fancy Feast.",
+        "pack": 2
+      },
+      {
+        "text": "Mac's spinning beach ball of death.",
+        "pack": 2
+      },
+      {
+        "text": "Michael Jackson's Neverland Ranch.",
+        "pack": 2
+      },
+      {
+        "text": "Exploding pigeons.",
+        "pack": 2
+      },
+      {
+        "text": "Not contributing to society in any meaningful way.",
+        "pack": 2
+      },
+      {
+        "text": "A crusty sock.",
+        "pack": 2
+      },
+      {
+        "text": "Tiger blood.",
+        "pack": 2
+      },
+      {
+        "text": "A balanced breakfast.",
+        "pack": 2
+      },
+      {
+        "text": "Losing your kidney in a dark alleyway.",
+        "pack": 2
+      },
+      {
+        "text": "Sharing needles.",
+        "pack": 2
+      },
+      {
+        "text": "Taking the Coding Dojo belt exam.",
+        "pack": 2
+      },
+      {
+        "text": "A drive-by shooting.",
+        "pack": 2
+      },
+      {
+        "text": "Talking about fight club.",
+        "pack": 2
+      },
+      {
+        "text": "Taking the red pill.",
+        "pack": 2
+      },
+      {
+        "text": "Wiping your partner's butt.",
+        "pack": 2
+      },
+      {
+        "text": "Dry heaving.",
+        "pack": 2
+      },
+      {
+        "text": "Free samples at Costco.",
+        "pack": 2
+      },
+      {
+        "text": "Pictures of scantily clad cats.",
+        "pack": 2
+      },
+      {
+        "text": "Friends with benefits.",
+        "pack": 2
+      },
+      {
+        "text": "Old Spice.",
+        "pack": 2
+      },
+      {
+        "text": "Mr. Roger's neighborhood.",
+        "pack": 2
+      },
+      {
+        "text": "Stranger danger.",
+        "pack": 2
+      },
+      {
+        "text": "The flying spaghetti monster.",
+        "pack": 2
+      },
+      {
+        "text": "Gettind drunk on cough medicine.",
+        "pack": 2
+      },
+      {
+        "text": "Natural selection.",
+        "pack": 2
+      },
+      {
+        "text": "Lunchables.",
+        "pack": 2
+      },
+      {
+        "text": "Knowing that big brother is always watching.",
+        "pack": 2
+      },
+      {
+        "text": "A bunch of idiots playing a card game instead of interacting like normal humans.",
+        "pack": 2
+      },
+      {
+        "text": "The Ten Commandments.",
+        "pack": 2
+      },
+      {
+        "text": "My Ex.",
+        "pack": 2
+      },
+      {
+        "text": "Playing a female RPG character so you can stare at those assets.",
+        "pack": 2
+      },
+      {
+        "text": "COVID-19.",
+        "pack": 2
+      },
+      {
+        "text": "The second coming of our lord and savior, Jesus Christ.",
+        "pack": 2
+      },
+      {
+        "text": "A fetish for sweaty people in the gym.",
+        "pack": 2
+      },
+      {
+        "text": "Finding a needle in a haystack.",
+        "pack": 2
+      },
+      {
+        "text": "The transatlantic slave trade.",
+        "pack": 2
+      },
+      {
+        "text": "Republicans.",
+        "pack": 2
+      },
+      {
+        "text": "A mime having a stroke.",
+        "pack": 2
+      },
+      {
+        "text": "The pink goop in Chicken McNuggets.",
+        "pack": 2
+      },
+      {
+        "text": "Black Friday at Walmart.",
+        "pack": 2
+      },
+      {
+        "text": "Bickering over the position of the north star.",
+        "pack": 2
+      },
+      {
+        "text": "in bed, pajama-clad 'intimate, non-sexual touching'.",
+        "pack": 2
+      },
+      {
+        "text": "A gourmet restaurant for dogs.",
+        "pack": 2
+      },
+      {
+        "text": "Pooping in a space suit.",
+        "pack": 2
+      },
+      {
+        "text": "Pinkeye.",
+        "pack": 2
+      },
+      {
+        "text": "Facebook's army of russian bots.",
+        "pack": 2
+      },
+      {
+        "text": "Evan and Leo, the dynamic duo!",
+        "pack": 2
+      },
+      {
+        "text": "Jenn pretending she's not on zoom.",
+        "pack": 2
+      },
+      {
+        "text": "James with ALL of the answers ALL of the time!",
+        "pack": 2
+      },
+      {
+        "text": "Nate tripping off his mountain bike down the side of a cliff.",
+        "pack": 2
+      },
+      {
+        "text": "Matthew using the code to destroy the code.",
+        "pack": 2
+      },
+      {
+        "text": "Deana breeding a horde of mutant lizards.",
+        "pack": 2
+      },
+      {
+        "text": "Sarah losing her ninty-ninth consecutive game in Warzone.",
+        "pack": 2
+      },
+      {
+        "text": "Tickling the ivory keys.",
+        "pack": 2
+      }
+    ],
+
+    "black": [
+      {
+        "text": "Aw babe, your burps smell like _.",
+        "pick": 1,
+        "pack": 0
+      },
+      {
+        "text": "Don`t fear failure, fear _",
+        "pick": 1,
+        "pack": 0
+      },
+      {
+        "text": "Excuse me, waiter. Could you take this back? This soup tastes like _.",
+        "pick": 1,
+        "pack": 0
+      },
+      {
+        "text": "I'm Bobby Flay, and if you can't stand _. get out of the kitchen!",
+        "pick": 1,
+        "pack": 0
+      },
+      {
+        "text": "It's not delivery. It's _.",
+        "pick": 1,
+        "pack": 0
+      },
+      {
+        "text": "Now on Netflix: _ Game.",
+        "pick": 1,
+        "pack": 0
+      },
+      {
+        "text": "_ , best job ever!",
+        "pick": 1,
+        "pack": 1
+      },
+      {
+        "text": "_: Achievement unlocked.",
+        "pick": 1,
+        "pack": 1
+      },
+      {
+        "text": "(Heavy breathing) Luke, I am _.",
+        "pick": 1,
+        "pack": 1
+      },
+      {
+        "text": "Press ↓ ↓ ← → B to unleash _.",
+        "pick": 1,
+        "pack": 1
+      },
+      {
+        "text": "What made Spock cry?",
+        "pick": 1,
+        "pack": 1
+      },
+      {
+        "text": "What's the latest gossip that's troubling this quaint fantasy town?",
+        "pick": 1,
+        "pack": 1
+      },
+      {
+        "text": "Computer! Display _ on screen. Enhance.",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "Feeling nostalgic when you remember _ from your childhood.",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "Madam President, the asteroid is headed directly for Earth and there's only one thing that can stop it: _.",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "This won't be like negotiating with the Vogons. Humans only respond to one thing: _.",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "What is the answer to life, the universe, and everything?",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "You have violated the Prime Directive! You exposed an alien culture to _ before they were ready.",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "The soup of the day is Cream of _ .",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "_ : good to the last drop.",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "Kids these days with their tablets and phones. In my day, all you needed was _ .",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "And the Academy Award goes to _ .",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "What's fun until it gets weird?",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "What's that smell?",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "What's my secret power?",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "_ . That's how I want to die.",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "Little Miss Muffet sat on a tuffet, eating her curds and _ .",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "Teach for America is using _ to inspire inner city kids to study.",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "Instead of coal, Santa now gives the bad children _ .",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "Instead of fifty lashings, you shall receive _ .",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "Maybe she's born with it. Maybe it's _ .",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "_ shall not pass!",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "Next time on Dr. Phil: How to talk to your child about _ .",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "_ . High-five bro.",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "_ for Dummies.",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "The latest Tiktok dance is _ .",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "Fortnite's next expansion: _ .",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "Trust me, I'm _ .",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "It's dangerous to go alone! Take _.",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "Set phasers to _ .",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "What gives me uncontrollable gas?",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "What's the newest diet craze?",
+        "pick": 1,
+        "pack": 2
+      },
+      {
+        "text": "You're not going to believe this, but I'm you from the future! You've got to stop _.",
+        "pick": 1,
+        "pack": 2
+      }
+    ],
+    "official": true
+  }
+] 
